462917a6b84b1201ec1c4da17258434300bbd938
ferutizi <fer.utizi@gmail.com>
2024-02-02 21:07:29 -0300

    the note that is currently playing is now visually activated

--- src/app/hooks/usePatternGenerator.ts
+++ src/app/hooks/usePatternGenerator.ts
@@ -1,8 +1,11 @@
+import { useState } from 'react'
 import { usePattern } from './usePattern'
 
 export default function usePatternGenerator() {
 	const [playNote] = usePattern() 
-	const randomNumbers: number[] = []
+	const [currentNote, setCurrentNote] = useState<number>(0)
+	const [randomNumbers, setRandomNumbers] = useState<number[]>([])
+	
 	const generateRandomNumber = () => {
 		randomNumbers[0]= Math.floor(Math.random() * 9) + 1
 		randomNumbers[1] = Math.floor(Math.random() * 9) + 1
@@ -11,15 +14,44 @@ export default function usePatternGenerator() {
 		randomNumbers[4] = 0
 	}
 
-	const playPattern = () => {
+	const patterns = [
+		[1, 1, 1, 1],
+		[1, 1, 3, 3],
+		[3, 3, 1, 1],
+		[1, 1, 5, 5],
+		[5, 5, 1, 1],
+		[1, 2, 1, 1],
+		[1, 3, 5, 5],
+		[5, 3, 1, 1],
+		[1, 2, 3, 4],
+		[5, 4, 3, 2]
+	]
+
+	const activeNote = (pattern) => {
 		let i = 0
+		const quaver = setInterval(() => {
+			setCurrentNote(patterns[pattern][i])
+			i++
+			if(i > 3) {
+				setCurrentNote(0)
+				clearInterval(quaver)
+			}
+		}, 320)
+	}
+
+	const playPattern = () => {
+		console.log('random number', randomNumbers)
+		let i = 0
+		console.log('random number en i', randomNumbers[i])
 		const compass = setInterval(() => {
 			playNote(randomNumbers[i])
+			activeNote(randomNumbers[i])
+			console.log(randomNumbers[i])
 			i++
 			if(i > 4) {
 				clearInterval(compass)
 			}
-		}, 1540)
+		}, 1550)
 	}
 
 	const generatePattern = () => {
@@ -27,5 +59,5 @@ export default function usePatternGenerator() {
 		playPattern()
 	} 
 
-	return[generatePattern, playPattern]
+	return[generatePattern, playPattern, currentNote] as const
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,22 @@
 'use client'
+
+import './components/Note.css'
 import usePatternGenerator from './hooks/usePatternGenerator'
-import Note from './components/Note'
 
 export default function Home() {
-	const [generatePattern, playPattern] = usePatternGenerator()
+	const [generatePattern, playPattern, currentNote] = usePatternGenerator()
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<h1>Singing Training</h1>
 			<button onClick={() => generatePattern()}>Start</button>
 			<button onClick={() => playPattern()}>Repeat</button>
 			<div className="flex gap-8">
-				<Note name='C'/>
-				<Note name='D'/>
-				<Note name='E'/>
-				<Note name='F'/>
-				<Note name='G'/>
+				<div className={`${currentNote === 1 && 'active'} flex justify-center items-center w-12 h-12 p-4 border border-solid border-gray-50`}>C</div>
+				<div className={`${currentNote === 2 && 'active'} flex justify-center items-center w-12 h-12 p-4 border border-solid border-gray-50`}>D</div>
+				<div className={`${currentNote === 3 && 'active'} flex justify-center items-center w-12 h-12 p-4 border border-solid border-gray-50`}>E</div>
+				<div className={`${currentNote === 4 && 'active'} flex justify-center items-center w-12 h-12 p-4 border border-solid border-gray-50`}>F</div>
+				<div className={`${currentNote === 5 && 'active'} flex justify-center items-center w-12 h-12 p-4 border border-solid border-gray-50`}>G</div>
 			</div>
 		</main>
 	)
