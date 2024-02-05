eb8d365fe42ecdfc39b4256b9cc8fc68b19b90ea
ferutizi <fer.utizi@gmail.com>
2024-02-05 13:29:51 -0300

    some styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,37 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './components/Note.css'
 import usePatternGenerator from './hooks/usePatternGenerator'
+import useCountdown from './hooks/useCountdown'
 
 export default function Home() {
 	const [help, setHelp] = useState<boolean>(true)
-	const [generatePattern, repeat, currentNote] = usePatternGenerator(help)
+	const [generatePattern, currentNote] = usePatternGenerator(help)
+	const [startCountdown, countdown, showCountdown] = useCountdown()
 
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1>Singing Training</h1>
-			<button onClick={() => generatePattern()}>Start</button>
-			<button onClick={() => setHelp(!help)}>help {help === true ? ' :on' : ' :off'}</button>
-			<button onClick={() => repeat()}>Repeat</button>
+			<h1 className='text-3xl'>Singing Training</h1>
+			{showCountdown &&
+				<p>{countdown}</p>
+			}
+			<div className='flex justify-between w-60'>
+				<button
+					onClick={() => setHelp(!help)}
+					className={`${help ? 'shadow-orange-300 border-orange-300 text-orange-300' : 'shadow-slate-600 border-slate-600 text-slate-400 shadow-sm'} px-4 py-2 border rounded-lg shadow-sm active:shadow-none`}
+				>Help {help === true ? ': ON' : ': OFF'}</button>
+				<button
+					onClick={() => {generatePattern(), startCountdown()}}
+					className='px-4 py-2 border rounded-lg shadow-sm shadow-slate-400 active:shadow-none'
+				>Start</button>
+			</div>
 			<div className="flex gap-8">
-				<div className={`${currentNote === 1 && 'active'} flex justify-center items-center w-12 h-12 text-xl p-4 mt-24 border border-solid bg-blue-800 border-gray-50`}>C</div>
-				<div className={`${currentNote === 2 && 'active'} flex justify-center items-center w-12 h-12 text-xl p-4 mt-20 border border-solid bg-gray-500 border-gray-50`}>D</div>
-				<div className={`${currentNote === 3 && 'active'} flex justify-center items-center w-12 h-12 text-xl p-4 mt-16 border border-solid bg-green-600 border-gray-50`}>E</div>
-				<div className={`${currentNote === 4 && 'active'} flex justify-center items-center w-12 h-12 text-xl p-4 mt-12 border border-solid bg-gray-500 border-gray-50`}>F</div>
-				<div className={`${currentNote === 5 && 'active'} flex justify-center items-center w-12 h-12 text-xl p-4 mt-8 border border-solid  bg-red-600 border-gray-50`}>G</div>
+				<div className={`${currentNote === 1 && 'active'} flex justify-center items-center w-12 h-12 text-xl p-4 mt-24 border rounded-lg bg-blue-800 border-gray-50`}>C</div>
+				<div className={`${currentNote === 2 && 'active'} flex justify-center items-center w-12 h-12 text-xl p-4 mt-20 border rounded-lg bg-gray-500 border-gray-50`}>D</div>
+				<div className={`${currentNote === 3 && 'active'} flex justify-center items-center w-12 h-12 text-xl p-4 mt-16 border rounded-lg bg-green-600 border-gray-50`}>E</div>
+				<div className={`${currentNote === 4 && 'active'} flex justify-center items-center w-12 h-12 text-xl p-4 mt-12 border rounded-lg bg-gray-500 border-gray-50`}>F</div>
+				<div className={`${currentNote === 5 && 'active'} flex justify-center items-center w-12 h-12 text-xl p-4 mt-8 border rounded-lg  bg-red-600 border-gray-50`}>G</div>
 			</div>
 		</main>
 	)
